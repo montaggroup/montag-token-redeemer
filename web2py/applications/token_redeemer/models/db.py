4f7c5baea90fbd60a87d577b80a13c98ff068357
dsk7 <jensg@posteo.de>
2015-08-08 11:35:23 +0200

    added web2py application without content

---
+++ web2py/applications/token_redeemer/models/db.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#########################################################################
+## This scaffolding model makes your app work on Google App Engine too
+## File is released under public domain and you can use without limitations
+#########################################################################
+
+## if SSL/HTTPS is properly configured and you want all HTTP requests to
+## be redirected to HTTPS, uncomment the line below:
+# request.requires_https()
+
+db = DAL('sqlite://storage.sqlite')
+
+## by default give a view/generic.extension to all actions from localhost
+## none otherwise. a pattern can be 'controller/function.extension'
+response.generic_patterns = ['*'] if request.is_local else []
+## (optional) optimize handling of static files
+# response.optimize_css = 'concat,minify,inline'
+# response.optimize_js = 'concat,minify,inline'
+
+#########################################################################
+## Here is sample code if you need for
+## - email capabilities
+## - authentication (registration, login, logout, ... )
+## - authorization (role based authorization)
+## - services (xml, csv, json, xmlrpc, jsonrpc, amf, rss)
+## - old style crud actions
+## (more options discussed in gluon/tools.py)
+#########################################################################
+
+from gluon.tools import Auth, Crud, Service, PluginManager
+
+auth = None
+
+crud, service, plugins = Crud(db), Service(), PluginManager()
